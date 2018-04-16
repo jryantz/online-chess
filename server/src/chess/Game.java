dbe973e1a2b7320b0369dc2d17ba08416cad88b6
Jonathan Yantz <ijryantz@gmail.com>
2018-04-16 15:58:54 -0400

    Added classes for handling chess game verification

---
+++ server/src/chess/Game.java
@@ -0,0 +1,7 @@
+package chess;
+
+public class Game {
+
+
+
+} // End class Game.
---
+++ server/src/chess/Move.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class Move {
+
+    public static boolean valid(char from, char to) {
+
+        return true;
+
+    } // end valid.
+
+} // end class Move.
