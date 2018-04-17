e978c99b6378ed576a33b13ad0449da2729d7c3a
Jonathan Yantz <ijryantz@gmail.com>
2018-04-17 16:40:11 -0400

    Client now functions as a communicator to the server

--- client/src/node/Client.java
+++ client/src/node/Client.java
@@ -2,10 +2,11 @@ package node;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
-    String fromClient;
+    DataOutputStream output;
 
     /**
      * Prepares the client for execution.
@@ -13,24 +14,75 @@ public class Client {
     public Client() {
 
         try {
-            Socket s = new Socket("127.0.0.1", 4000);
+            Socket sock = new Socket("127.0.0.1", 4000);
 
-            // Sending to server.
-            DataOutputStream output = new DataOutputStream(s.getOutputStream());
-            //output.writeBytes("Test...\n");
-            output.writeBytes("--move id piece A1 A2\n");
+            System.out.println("Chess Client Started...");
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            // Create the receiver thread.
+            new Connection(sock);
 
-            while (true) {
-                if((fromClient = in.readLine()) != null) {
-                    System.out.println(fromClient);
-                }
+            // Setup the output data stream.
+            try {
+                output = new DataOutputStream(sock.getOutputStream());
+            } catch (IOException e) {
+                System.out.println("Connection: " + e.getMessage());
+            }
+
+            // Scanner for debug testing (passing test messages to the server).
+            Scanner s = new Scanner(new InputStreamReader(System.in));
+            while(s.hasNextLine()) {
+                output.writeBytes(s.nextLine() + "\n");
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-    }
+    } // end Client.
 
-}
+} // end class Client.
+
+class Connection extends Thread {
+
+    Socket serverSocket;
+    String fromClient;
+
+    /**
+     * Constructor for building the connection.
+     * Prepares the client socken and the input/output stream.
+     * Starts the thread.
+     *
+     * @param incomingSocket the socket that connects to the server.
+     */
+    public Connection (Socket incomingSocket) {
+
+        serverSocket = incomingSocket;
+
+        this.start();
+
+    } // end Connection.
+
+    /**
+     * Begin execution of the receiver thread.
+     */
+    public void run() {
+
+        boolean cont = true;
+
+        try {
+            while (cont) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+
+                if((fromClient = in.readLine()) != null) {
+                    System.out.println(fromClient);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error.");
+            e.printStackTrace();
+        }
+
+        System.exit(0);
+
+    } // end run.
+
+} // end class Connection.
