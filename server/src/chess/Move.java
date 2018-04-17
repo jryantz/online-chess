5e0b02d5173306eba0d234f0a7185ef65e7f4c33
Jonathan Yantz <ijryantz@gmail.com>
2018-04-17 15:16:59 -0400

    Added code to show how piece checking would work

--- server/src/chess/Move.java
+++ server/src/chess/Move.java
@@ -2,10 +2,29 @@ package chess;
 
 public class Move {
 
-    public static boolean valid(char from, char to) {
+    public static boolean valid(String piece, char from, char to) {
 
-        return true;
+        switch(piece) {
+            case "King":
+                return kingMove(from, to);
+            case "Queen":
+                return queenMove(from, to);
+            default:
+                return false;
+        }
 
     } // end valid.
+
+    private static boolean kingMove(char from, char to) {
+        return false;
+    } // end kingMove.
+
+    private static boolean queenMove(char from, char to) {
+        return false;
+    } // end queenMove.
+
+    private static boolean checkCheck() {
+        return false;
+    } // end checkCheck.
 
 } // end class Move.
